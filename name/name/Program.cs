0c8c889cec6ed75945d9f832a4742e47cfa9777f
TinaOporto <cristina.oporto@ymail.com>
2015-11-20 20:35:37 +0800

    program2

---
+++ name/name/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Firstname: ");
+            string fname = Console.ReadLine();
+
+            Console.WriteLine("Enter Lastname: ");
+            string lname = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your fullname is: " + fname + " " + lname );
+            Console.WriteLine("Your age is: " + age + " years old");
+
+            Console.ReadKey();
+        }
+    }
+}
